1b9ce945138957d9cfe8263bd1cb147513eb2cee
faiazrahman <faiaz.rahman@yale.edu>
2021-12-13 15:54:42 -0500

    init dialogue model and trainer

--- dialogue_experiments.py
+++ dialogue_experiments.py
@@ -404,6 +404,143 @@ class MultimodalFakeNewsDetectionModel(pl.LightningModule):
             dropout_p=self.hparams.get("dropout_p", DROPOUT_P)
         )
 
+class JointTextImageDialogueModel(nn.Module):
+
+    def __init__(
+            self,
+            num_classes,
+            loss_fn,
+            text_module,
+            image_module,
+            text_feature_dim,
+            image_feature_dim,
+            fusion_output_size,
+            dropout_p,
+            hidden_size=512,
+        ):
+        super(JointVisualTextualModel, self).__init__()
+        self.text_module = text_module
+        self.image_module = image_module
+        self.fusion = torch.nn.Linear(in_features=(text_feature_dim + image_feature_dim),
+            out_features=fusion_output_size)
+        # self.fc = torch.nn.Linear(in_features=fusion_output_size, out_features=num_classes)
+        self.fc1 = torch.nn.Linear(in_features=fusion_output_size, out_features=hidden_size) # trial
+        self.fc2 = torch.nn.Linear(in_features=hidden_size, out_features=num_classes) # trial
+        self.loss_fn = loss_fn
+        self.dropout = torch.nn.Dropout(dropout_p)
+
+    def forward(self, text, image, label):
+        text_features = torch.nn.functional.relu(self.text_module(text))
+        image_features = torch.nn.functional.relu(self.image_module(image))
+        # print(text_features.size(), image_features.size()) # torch.Size([32, 300]) torch.Size([16, 300])
+        combined = torch.cat([text_features, image_features], dim=1)
+        fused = self.dropout(
+            torch.nn.functional.relu(self.fusion(combined))) # TODO add dropout
+        # logits = self.fc(fused)
+        hidden = torch.nn.functional.relu(self.fc1(fused)) # trial
+        logits = self.fc2(hidden) # trial
+        # pred = torch.nn.functional.softmax(logits, dim=1)
+        pred = logits # nn.CrossEntropyLoss expects raw logits as model output # https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
+        loss = self.loss_fn(pred, label)
+        return (pred, loss)
+
+class MultimodalFakeNewsDetectionModelWithDialogue(pl.LightningModule):
+
+    def __init__(self, hparams=None):
+        super(MultimodalFakeNewsDetectionModel, self).__init__()
+        if hparams:
+            self.hparams.update(hparams) # https://github.com/PyTorchLightning/pytorch-lightning/discussions/7525
+
+        self.embedding_dim = self.hparams.get("embedding_dim", 768)
+        self.text_feature_dim = self.hparams.get("text_feature_dim", 300)
+        self.image_feature_dim = self.hparams.get("image_feature_dim", self.text_feature_dim)
+
+        self.model = self._build_model()
+
+    # Required for pl.LightningModule
+    def forward(self, text, image, label):
+        # pl.Lightning convention: forward() defines prediction for inference
+        return self.model(text, image, label)
+
+    # Required for pl.LightningModule
+    def training_step(self, batch, batch_idx):
+        global losses
+        # pl.Lightning convention: training_step() defines prediction and
+        # accompanying loss for training, independent of forward()
+        text, image, label = batch["text"], batch["image"], batch["label"]
+        # print(image.size())
+        # print(text, image, label)
+
+        pred, loss = self.model(text, image, label)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        print(loss.item())
+        losses.append(loss.item())
+        return loss
+
+    def training_step_end(self, batch_parts):
+        """
+        Aggregates results when training using a strategy that splits data
+        from each batch across GPUs (e.g. data parallel)
+
+        Note that training_step returns a loss, thus batch_parts returns a list
+        of 2 loss values (since there are 2 GPUs being used)
+        """
+        return sum(batch_parts) / len(batch_parts)
+
+    def test_step(self, batch, batch_idx):
+        text, image, label = batch["text"], batch["image"], batch["label"]
+        pred, loss = self.model(text, image, label)
+        pred_label = torch.argmax(pred, dim=1)
+        accuracy = torch.sum(pred_label == label).item() / (len(label) * 1.0)
+        output = {
+            'test_loss': loss,
+            'test_acc': torch.tensor(accuracy).cuda()
+        }
+        print(loss.item(), output['test_acc'])
+        return output
+
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
+        avg_accuracy = torch.stack([x["test_acc"] for x in outputs]).mean()
+        logs = {
+            'test_loss': avg_loss,
+            'test_acc': avg_accuracy
+        }
+        self.test_results = logs # https://github.com/PyTorchLightning/pytorch-lightning/issues/1088
+        return {
+            'avg_test_loss': avg_loss,
+            'avg_test_acc': avg_accuracy,
+            'log': logs,
+            'progress_bar': logs
+        }
+
+    # Required for pl.LightningModule
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=LEARNING_RATE)
+        # optimizer = torch.optim.SGD(self.parameters(), lr=LEARNING_RATE, momentum=0.9)
+        return optimizer
+
+    def _build_model(self):
+        text_module = torch.nn.Linear(
+            in_features=self.embedding_dim, out_features=self.text_feature_dim)
+
+        image_module = torchvision.models.resnet152(pretrained=True)
+        # Overwrite last layer to get features (rather than classification)
+        image_module.fc = torch.nn.Linear(
+            in_features=RESNET_OUT_DIM, out_features=self.image_feature_dim)
+
+        return JointTextImageDialogueModel(
+            num_classes=self.hparams.get("num_classes", NUM_CLASSES),
+            loss_fn=torch.nn.CrossEntropyLoss(),
+            text_module=text_module,
+            image_module=image_module,
+            text_feature_dim=self.text_feature_dim,
+            image_feature_dim=self.image_feature_dim,
+            fusion_output_size=self.hparams.get("fusion_output_size", 512),
+            dropout_p=self.hparams.get("dropout_p", DROPOUT_P)
+        )
+
+
 class PrintCallback(Callback):
     def on_train_start(self, trainer, pl_module):
         print("Training started...")
@@ -462,7 +599,7 @@ if __name__ == "__main__":
     print("test: ", len(test_dataset))
     print(train_dataset[0])
 
-    ###
+    ### DIALOGUE DATA
     # df = train_dataset.data_frame
     # print(df.columns)
     # print(df['id'])
