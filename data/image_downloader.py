60993bacb3558f281081dbd1958514e031e3b02e
faiazrahman <faiaz.rahman@yale.edu>
2021-12-14 12:07:54 -0500

    note on data download script

--- data/image_downloader.py
+++ data/image_downloader.py
@@ -1,4 +1,17 @@
-# https://github.com/entitize/Fakeddit
+"""
+This script is a modified version of the original image_downloader.py script
+provided by the Fakeddit dataset
+
+https://github.com/entitize/Fakeddit
+
+The changes are minor: I wrapped the urllib.request in a try-except to avoid
+having errors stop the entire download process, and I avoided redownloading
+images which have already been downloaded by checking if the image already
+exists (e.g. if a previous run failed or only downloaded part of the data)
+
+Note that the text data must be downloaded first; see the Fakeeddit GitHub repo
+for instructions on how to download the text (and the comment dialogue) data
+"""
 
 import argparse
 import pandas as pd
@@ -23,16 +36,16 @@ pbar = tqdm(total=len(df))
 num_failed = 0
 
 if not os.path.exists("images"):
-  os.makedirs("images")
+    os.makedirs("images")
 for index, row in df.iterrows():
-  if row["hasImage"] == True and row["image_url"] != "" and row["image_url"] != "nan":
-    image_url = row["image_url"]
-    try:
-      image_path = os.path.join("images", row["id"] + ".jpg")
-      if not os.path.exists(image_path):
-        urllib.request.urlretrieve(image_url, "images/" + row["id"] + ".jpg")
-    except:
-      num_failed += 1
-  pbar.update(1)
+    if row["hasImage"] == True and row["image_url"] != "" and row["image_url"] != "nan":
+        image_url = row["image_url"]
+        try:
+            image_path = os.path.join("images", row["id"] + ".jpg")
+            if not os.path.exists(image_path):
+                urllib.request.urlretrieve(image_url, "images/" + row["id"] + ".jpg")
+        except:
+            num_failed += 1
+        pbar.update(1)
 print("done")
 print("num_failed: {}".format(num_failed))
